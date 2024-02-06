c7c6f8182891f93930419379b98bb75ddbddc018
hura2 <hura2>
2024-02-06 14:20:38 +0900

    冬の時期は暖かい服装で固定

--- src/home_climate_control.py
+++ src/home_climate_control.py
@@ -29,8 +29,14 @@ def calculate_met_icl(outdoor_temperature: float, bedtime: bool):
     else:
         met = 1.0 if bedtime else 1.1
 
-        icl_daytime = max(1.1 - 0.03 * max(min(ot, 19) - 9, 0), 0.6)
-        icl_bedtime = max(1.6 - 0.06 * max(min(ot, 19) - 9, 0), 0.8)
+        # 冬の時期は暖かい服装で固定
+        if 12 <= now.month or 2 >= now.month:
+            icl_daytime = 1.1
+            icl_bedtime = 1.6
+        else:
+            icl_daytime = max(1.1 - 0.03 * max(min(ot, 15) - 9, 0), 0.6)
+            icl_bedtime = max(1.6 - 0.06 * max(min(ot, 15) - 9, 0), 0.8)
+
         icl = icl_bedtime if bedtime else icl_daytime
 
         # 8時からは電気代が高くなるので暖房を抑制
