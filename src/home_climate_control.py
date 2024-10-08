e5747261bd197f012a217d505ea43b3556b391ef
hura2 <135929251+hura2@users.noreply.github.com>
2024-10-09 03:51:51 +0900

    Update home_climate_control.py

--- src/home_climate_control.py
+++ src/home_climate_control.py
@@ -14,8 +14,8 @@ def calculate_met_icl(outdoor_temperature: float, max_temp: int, bedtime: bool):
     # 現在の曜日を取得
     current_day = now.weekday()  # 0:月曜, 1:火曜, ..., 6:日曜
     ot = outdoor_temperature
-    # 最高気温が25度以上は涼しい服装
-    if max_temp >= 25:
+    # 最高気温が30度以上は涼しい服装
+    if max_temp >= 30:
         met = 1.0 if bedtime else 1.1
         icl = 0.8 if bedtime else 0.6
         # たくさん活動する時間帯はmetを増やす
